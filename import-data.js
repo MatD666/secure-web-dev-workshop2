6afffbe09393be3c2703d77082e7a31bb1047f78
MatD666 <dupriez.matteo@gmail.com>
2022-09-27 20:16:16 +0200

    second

--- import-data.js
+++ import-data.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 console.log(process.env)
 const filmingLocations = require('./lieux-de-tournage-a-paris.json')
 
-import mongoose from 'mongoose';
+
+const mongoose = require('mongoose')
 const { Schema } = mongoose;
 
 const filmLocationShema = new Schema({
@@ -21,4 +22,32 @@ const filmLocationShema = new Schema({
     year: Number,
 });
 
-const Locations = mongoose.model('locations',filmLocationShema)
+
+//mongoose.connect(process.env.MONGO_URI).then(()=> {console.log('Connected !')});
+async function main () {
+    const Locations = mongoose.model('locations',filmLocationShema);
+    const result = await mongoose.connect(process.env.MONGO_URI);
+    console.log(result);
+    //const maPremiereLocation = new Locations({filmType:'Horror'});
+    //await maPremiereLocation.save();
+    for(X in filmingLocations){
+        const Local = new Locations({
+            filmType: filmingLocations[X].fields.nom_tournage,
+            filmProducerName: filmingLocations[X].fields.nom_tournage,
+            endDate:   Date,
+            filmName: filmingLocations[X].fields.nom_tournage,
+            district: filmingLocations[X].fields.nom_tournage,
+            geolocation:{coordinates:[Number],
+                type: String
+            },
+            sourceLocationId: String,
+            filmDirectorName: String,
+            address: String,
+            startDate: Date,
+            year: Number,
+        })
+    }
+    console.log('I am finish ♥')
+}
+
+main();
